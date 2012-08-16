22f240ec404d3acdbf7d412128a5e55b6812e5f7
woody1983 <unix1983@gmail.com>
2012-08-16 11:53:58 +0800

    Update about rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -81,3 +81,80 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   end
 
 end
+
+
+describe "StaticPages Method2" do
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading)    { 'Phoenix OA' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Home' }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+    let(:heading) {'Help'}
+    let(:page_title) {''}
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Help' }
+  end
+
+  describe "About page" do
+    before {visit about_path }
+    let(:heading) {'About Us'}
+    let(:page_title){'About Us'}
+    it_should_behave_like "all static pages"
+    it { should have_selector 'title', text: '| About Us' }
+  end
+
+  describe "Contact" do
+    before { visit contact_path }
+    let(:heading) {'Contact'}
+    let(:page_title) {'Contact'}
+    it_should_behave_like "all static pages"
+    it { should have_selector 'title', text: '| Contact' }
+  end
+
+  it "should have the right links on the layout" do
+  visit root_path
+  click_link "About"
+  page.should have_selector 'title', text: full_title('About Us')
+  click_link "Help"
+  page.should_not have_selector 'title', text: full_title('Help')
+  page.should have_selector 'title', text: full_title('')
+  click_link "Contact"
+  page.should have_selector 'title', text: full_title('Contact')
+  click_link "Home"
+  click_link "Sign up now!"
+  page.should have_selector 'title', text: full_title('Sign up')
+  page.should have_selector 'h1', text: 'Sign up'
+  click_link "Phoenix OA"
+  page.should have_selector 'title', text: full_title('')
+  end
+ 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
