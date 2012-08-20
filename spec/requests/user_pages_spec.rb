f7647766500959a1b5ff6596913598ee480946f7
woody1983 <unix1983@gmail.com>
2012-08-20 22:51:18 +0800

    Add SSL in production & rspec user

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,11 +21,19 @@ describe "User pages" do
   describe "signup" do
     before { visit signup_path }
     let(:submit) { "Create my account" }
+
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      describe "after submission" do
+        before { click_button submit } #什么都不写 直接提交
+          it { should have_selector('title', :text=>full_title('Sign up')) }
+	  it { should have_content('error') }
+          it { should have_content('The form contains 7 errors.') } #返回的标准错误
+      end
     end
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -36,7 +44,17 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-    end
-  end
+      describe "after saving the user" do
+        before { click_button submit }
+	let(:user) { User.find_by_email('user@example.com') }
+        #save以后 by email去数据库把user信息抓出来 和注册成功后的页面信息作对比
+	it { should have_selector('title', text: user.name) }
+	it { should have_selector('h1', text: user.name) }
+	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
 
-end
+    end #with valid information
+
+  end #signup
+
+end #User pages
